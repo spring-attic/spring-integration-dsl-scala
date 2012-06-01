ded52724601e9d8e368091c9fd32d38f5864e764
Oleg Zhurakousky <ozhurakousky@vmware.com>
2012-06-01 17:22:33 -0400

    TESTFIX fixed test and added C24 demos

---
+++ spring-integration-dsl-scala-integration-demos/src/test/scala/scala/c24/demo/java/C24Validator.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2002-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scala.c24.demo.java;
+/**
+ * 
+ * @author Oleg Zhurakousky
+ * 
+ */
+import java.io.ByteArrayInputStream;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+
+
+public class C24Validator {
+	
+	public boolean isValid(C24Object c24) throws Exception {
+		Document document = DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder()
+				.parse(new ByteArrayInputStream(c24.getXml().getBytes()));
+
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		XPathExpression xpathExpression = xpath.compile("//transaction/value");
+		String result = xpathExpression.evaluate(document).toLowerCase();
+		
+		return result.contains("valid");
+	}
+
+}
